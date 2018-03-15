8f1eb16e4954a0feb69e2272c08ba6292647eb11
FOLFO93 <uv.adolfo@hotmail.com>
2018-03-15 21:12:10 +0100

    Insercion del segundo Fragment

--- app/src/main/java/com/example/folfo93/fragmentskotlin/MainFragment.kt
+++ app/src/main/java/com/example/folfo93/fragmentskotlin/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.folfo93.fragmentskotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -16,17 +17,24 @@ class MainFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_main, container)
     }
 
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //Le damos funcionalidad al boton
         main_button.setOnClickListener {
+            //Mostramos toast
             toast("Mensaje: ${main_editText.text}")
             main_textview.text = main_editText.text
+
+            //Utilizamos 'activity' para indicar el context; al ser un fragment, el context es la activity que lo contiene.
+            val datos = Intent(activity,Main2Activity::class.java)
+            datos.putExtra("key1","valor1")
+            startActivity(datos)
         }
     }
 }
